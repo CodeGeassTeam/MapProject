ad5b7bf26bc53f9d7552bcdacc0a6297be6214b3
ProbablyDannie <abbicando@gmail.com>
2025-04-20 21:54:59 -0400

    Incomplete models

--- App/models/user.py
+++ App/models/user.py
@@ -1,7 +1,32 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
-class User(db.Model):
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username =  db.Column(db.String(20), nullable=False, unique=True)
+#     password = db.Column(db.String(120), nullable=False)
+
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.set_password(password)
+
+#     def get_json(self):
+#         return{
+#             'id': self.id,
+#             'username': self.username
+#         }
+
+#     def set_password(self, password):
+#         """Create hashed password."""
+#         self.password = generate_password_hash(password)
+    
+#     def check_password(self, password):
+#         """Check hashed password."""
+#         return check_password_hash(self.password, password)
+
+class Admin(db.Model):
+    __tablename__ = 'admins'
+
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
@@ -23,6 +48,52 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
+
+    # def addMarker(self, markerdetails):
+    #     new_todo = Todo(text=text)
+    #     new_todo.user_id = self.id
+    #     self.todos.append(new_todo)
+    #     db.session.add(self)
+    #     db.session.commit()
+    #     return new_marker
+
+#consider string for ids?
+
+
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+    buildings = db.relationship('BuildingDetails', back_populates='location', cascade='all, delete-orphan')
+
+
+class BuildingDetails(db.Model):
+    __tablename__ = 'building_details'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    num_floors = db.Column(db.Integer, nullable=False)
+
+    location = db.relationship('Location', back_populates='buildings')
+    rooms = db.relationship('Room', back_populates='building', cascade='all, delete-orphan')
+
+
+class Room(db.Model):
+    __tablename__ = 'rooms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    building_id = db.Column(db.Integer, db.ForeignKey('building_details.id'), nullable=False)
+    floor = db.Column(db.Integer, nullable=False)
+    room_number = db.Column(db.String(50), nullable=False)
+    room_name = db.Column(db.String(100), nullable=True)
+    faculty = db.Column(db.String(100), nullable=True)
+
+    building = db.relationship('BuildingDetails', back_populates='rooms')
+
+
 
 class Marker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
