2b34a9942eedf27555e2c5ef43b1ffb6242c13ca
dylann-s <157635473+dylann-s@users.noreply.github.com>
2025-04-21 21:24:32 -0400

    database markers

--- App/views/index.py
+++ App/views/index.py
@@ -4,10 +4,18 @@ from App.models import *
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
+##########################################################################
 @index_views.route('/', methods=['GET'])
 def index_page():
     markers = Marker.query.all()
-    return render_template('home.html')#, markers=markers)
+    markers_data = [{
+        'lat': marker.lat,
+        'lon': marker.lon,
+        'building': marker.building.name if marker.building else '',
+        'room': marker.room.room_number if marker.room else ''
+    } for marker in markers]
+    return render_template('home.html', markers_json=markers_data)
+##########################################################################
 
 @index_views.route('/init', methods=['GET'])
 def init():
